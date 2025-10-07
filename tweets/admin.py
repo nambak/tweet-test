91daa18816a7542200081783bab95b7089d10499
Alex Nam <nambak80@gmail.com>
2025-10-07 20:37:39 +0900

    Assignment 7
    - Like 모델 어드민 개선
    - Tweet 모델 어드민 개선

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
 from .models import Tweet, Like
 
+class ElonMuskFilter(admin.SimpleListFilter):
+    title = 'Elon Musk'
+    parameter_name = 'elon_musk'
+
+    def lookups(self, request, model_admin):
+        return [('yes', 'Contains'), ('no', 'Does not contain')]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'yes':
+            return queryset.filter(payload__icontains='Elon Musk')
+        if self.value() == 'no':
+            return queryset.exclude(payload__icontains='Elon Musk')
 
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'payload', 'like_count', 'created_at', 'updated_at')
     readonly_fields = ('created_at', 'updated_at')
+    search_fields = ('payload', 'user__username')
+    list_filter = ('created_at', ElonMuskFilter)
 
     def like_count(self, obj):
         return obj.likes.count()
@@ -16,3 +30,5 @@ class TweetAdmin(admin.ModelAdmin):
 class LikeAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'tweet', 'created_at')
     readonly_fields = ('created_at', 'updated_at')
+    list_filter = ('created_at',)
+    search_fields = ('user__username',)

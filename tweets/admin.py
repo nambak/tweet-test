1c96acbe251060db08f136e5cce4d866ec2fc6e7
Alex Nam <nambak80@gmail.com>
2025-10-07 20:26:49 +0900

    list_filter, search_fields 초기화

--- tweets/admin.py
+++ tweets/admin.py
@@ -5,8 +5,6 @@ from .models import Tweet, Like
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'payload', 'like_count', 'created_at', 'updated_at')
-    list_filter = ('created_at', 'user')
-    search_fields = ('payload', 'user__username')
     readonly_fields = ('created_at', 'updated_at')
 
     def like_count(self, obj):
@@ -17,6 +15,4 @@ class TweetAdmin(admin.ModelAdmin):
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'tweet', 'created_at')
-    list_filter = ('created_at', 'user')
-    search_fields = ('user__username', 'tweet__payload')
     readonly_fields = ('created_at', 'updated_at')

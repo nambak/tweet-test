89532e119ac9e2d018a80d143ddf677db18b0ec6
Alex Nam <nambak80@gmail.com>
2025-10-09 01:53:06 +0900

    Assignment 8
    urls.py, views.py, render() 그리고 Django ORM 을 활용하여. / 페이지에 유저가 방문하였을때. 모든 Tweets 을 보여주는 리스트 작성.

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tweets import views
 
 urlpatterns = [
+    path("", views.tweet_list, name="tweet_list"),
     path('admin/', admin.site.urls),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Tweet
 
-# Create your views here.
+
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by("-created_at")
+    return render(request, "tweets/tweet_list.html", {"tweets": tweets})
